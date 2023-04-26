7a9c32cfb89f3149404116084c607568aa7ee28c
Ayoub <ayoubaouinti1990@gmail.com>
2023-04-26 10:16:35 +0200

    five commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ResitrationListComponent } from './resitration-list/resitration-list.component';
+import { CreateResitrationComponent } from './create-resitration/create-resitration.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'register', pathMatch:'full'},
+  {path: 'register', component:CreateResitrationComponent},
+  {path: 'list', component:ResitrationListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
